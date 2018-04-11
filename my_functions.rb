af9d235a2d294d5ff600a61ed485394acd3125eb
Richard Ramson <richard.ramson.jam@gmail.com>
2018-04-11 15:46:06 +0100

    inital commit

---
+++ my_functions.rb
@@ -0,0 +1,7 @@
+def add_array_lengths(array1, array2)
+  return array1.length + array2.length
+end
+
+
+  # Write a function that takes has two parameters, array1 and array2
+  # It should return the sum of the array lengths
